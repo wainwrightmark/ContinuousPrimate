f0fac9dce7c2106606cc259f7c764ec6a2e4706f
Mark Wainwright <wainwrightml@gmail.com>
2022-01-29 01:37:38 +0000

    Fixed some small issues

--- ContinuousPrimate.Test/IntegrationTests.cs
+++ ContinuousPrimate.Test/IntegrationTests.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using MoreLinq.Experimental;
+using WordNet;
 using Xunit;
 using Xunit.Abstractions;
 //using WordNet;
@@ -14,6 +17,29 @@ public class IntegrationTests
     }    
 
     public ITestOutputHelper TestOutputHelper { get; }
+
+
+    private static Lazy<IEnumerable<(AnagramKey key, string name)>> FirstNames= new(
+
+        Words.Names
+            .Split('\n', StringSplitOptions.TrimEntries)
+            .Select(name=> (AnagramKey.Create(name), name)).Memoize()
+            
+    );
+    private static Lazy<ILookup<AnagramKey, string>> NounLookup = new(()=>
+        Words.Nouns
+            .Split('\n', StringSplitOptions.TrimEntries)
+            .ToLookup(AnagramKey.Create)
+    );
+    private static Lazy<ILookup<AnagramKey, string>> AdjectiveLookup = new(()=>
+        Words.Adjectives
+            .Split('\n', StringSplitOptions.TrimEntries)
+            .ToLookup(AnagramKey.Create)
+    );
+    
+        
+
+
 
     [Theory]
     [InlineData("beckham")]
@@ -33,37 +59,41 @@ public class IntegrationTests
     [InlineData("thomas")]
     public void TestSearch(string mainName)
     {
-        throw new NotImplementedException();
-        //var results = 
-        //NameSearch.GetPartialAnagrams(mainName
-        //);
 
-        //foreach (var partialAnagram in results)
-        //{
-        //    var text = $"{partialAnagram.FullName} = {partialAnagram.Anagram}";
+        var results =
+        NameSearch.FindNameAnagrams(mainName,
+                FirstNames.Value,
+                NounLookup.Value,
+                AdjectiveLookup.Value
+        );
 
-        //    TestOutputHelper.WriteLine(text);
-        //}
+        foreach (var partialAnagram in results)
+        {
+            var text = $"{partialAnagram.FullName} = {partialAnagram.Anagram}";
+
+            TestOutputHelper.WriteLine(text);
+        }
     }
 
-    //[Theory]
-    //[InlineData(PartOfSpeech.Adjective)]
-    //[InlineData(PartOfSpeech.Noun)]
-    //public void TestPartOfSpeech(PartOfSpeech partOfSpeech)
-    //{
-    //    var wordNetEngine = new WordNetEngine();
+    [Theory]
+    [InlineData(PartOfSpeech.Adjective)]
+    [InlineData(PartOfSpeech.Noun)]
+    public void TestPartOfSpeech(PartOfSpeech partOfSpeech)
+    {
+        var wordNetEngine = new WordNetEngine();
 
-    //    var words = wordNetEngine.GetAllSynSets()
-    //        .Where(x => x.PartOfSpeech == partOfSpeech)
-    //        .SelectMany(x=>x.Words)
-    //        .Where(x=> x.All(c=> char.IsLetter(c) && char.IsLower(c) ))
-    //        .Distinct().ToList();
+        var words = wordNetEngine.GetAllSynSets()
+            .Where(x => x.PartOfSpeech == partOfSpeech)
+            .SelectMany(x => x.Words)
+            .Where(x => x.All(c => char.IsLetter(c) && char.IsLower(c)))
+            .Except(FirstNames.Value.Select(x=>x.name))
+            .Distinct().ToList();
 
-    //    TestOutputHelper.WriteLine(words.Count + " Words");
+        TestOutputHelper.WriteLine(words.Count + " Words");
 
-    //    foreach (var adjective in words)
-    //    {
-    //        TestOutputHelper.WriteLine(adjective);
-    //    }
-    //}
+        foreach (var adjective in words)
+        {
+            TestOutputHelper.WriteLine(adjective);
+        }
+    }
 }
