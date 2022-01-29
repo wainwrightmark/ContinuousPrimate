feb1096b070944e2651c03c540bc681550ae2983
Mark Wainwright <wainwrightml@gmail.com>
2022-01-29 13:36:05 +0000

    Added Browser Storage

--- ContinuousPrimate.Blazor/Program.cs
+++ ContinuousPrimate.Blazor/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -9,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 
 
 await builder.Build().RunAsync();
--- ContinuousPrimate/NameSearch.cs
+++ ContinuousPrimate/NameSearch.cs
@@ -207,17 +207,24 @@ public readonly record struct AnagramKey(string Text)
     
 }
 
-
-
-
 public record PartialAnagram(IReadOnlyList<string> OriginalTerms, IReadOnlyList<Word> AnagramWords)
 {
     /// <inheritdoc />
-    public override string ToString()
-    {
-        return TermsText + " = " + AnagramText;
-    }
+    public override string ToString() => Data;
 
     public string TermsText => string.Join(" ", OriginalTerms);
     public string AnagramText => string.Join(" ", AnagramWords.Select(x => x.Text));
+
+    public string Data =>TermsText + " = " + AnagramText;
+
+    /// <inheritdoc />
+    public override int GetHashCode() => Data.GetHashCode();
+
+    /// <inheritdoc />
+    public virtual bool Equals(PartialAnagram? other)
+    {
+        if (other is null) return false;
+
+        return Data == other.Data;
+    }
 }
